2e66ed5d5daf8600870cd676297e2d91a5fbe812
= <=>
2020-01-11 23:43:51 +0700

    React Router and Routing

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
 import React from 'react';
 import './App.css';
-
+import {Route, Switch, Link} from 'react-router-dom';
 import HomePage from './pages/homepage/homepage.component';
+
+const HatsPage = () =>(
+  <div>
+    <h1>HATS PAGE</h1>
+    <Link to="/">Homepage</Link>
+  </div>
+)
 
 function App() {
   return (
     <div className="App">
-        <HomePage/>>
+      <Switch>
+        <Route exact path="/" component={HomePage}/>
+        <Route  path="/hats" component={HatsPage}/>
+      </Switch>
+
     </div>
   );
 }
