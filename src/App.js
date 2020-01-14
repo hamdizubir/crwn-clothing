a406695478d16f96f90d5d3b10cc390466cb0e99
Hamdi Zubir Koto <hamdizubir_k@Kotos-Mac-mini.local>
2020-01-14 19:29:11 +0700

    Header Component

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import './App.css';
 import {Route, Switch, Link} from 'react-router-dom';
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
+import Header from './components/header/header.component';
+// import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
 
 const HatsPage = () =>(
   <div>
@@ -14,9 +16,11 @@ const HatsPage = () =>(
 function App() {
   return (
     <div className="App">
+      <Header/>
       <Switch>
         <Route exact path="/" component={HomePage}/>
         <Route  path="/shop" component={ShopPage}/>
+        {/* <Route  path="/signin" component={SignInAndSignUpPage}/> */}
       </Switch>
 
     </div>
