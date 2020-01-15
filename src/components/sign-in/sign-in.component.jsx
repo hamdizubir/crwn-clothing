5cea006e47482a1930ebd9625c7d6c66f38a4033
Hamdi Zubir Koto <hamdizubir_k@Kotos-Mac-mini.local>
2020-01-15 15:00:51 +0700

    Sign in component

---
+++ src/components/sign-in/sign-in.component.jsx
@@ -0,0 +1,57 @@
+import React, { Component } from "react";
+import "./sign-in.styles.scss";
+
+class SignIn extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      email: "",
+      password: ""
+    };
+  }
+
+  handleSubmit = event => {
+    event.preventDefault();
+
+    this.setState({ email: "", password: "" });
+  };
+
+  handleChange = event =>{
+    const {value, name} = event.target;
+
+    this.setState({[name]:value});
+  }
+
+  render() {
+    return (
+      <div className="sign-in">
+        <h2>I Already have an account</h2>
+        <span>Sign in with email and password</span>
+
+        <form>
+          <input
+            onChange={this.handleChange}
+            name="email"
+            type="email"
+            value={this.state.email}
+            required
+          />
+          <label>Email</label>
+          <input
+            onChange={this.handleChange}
+            name="password"
+            type="password"
+            value={this.state.password}
+            required
+          />
+          <label>Password</label>
+
+          <input type="submit" value="Submit Form" />
+        </form>
+      </div>
+    );
+  }
+}
+
+export default SignIn;
--- src/pages/sign-in-and-sign-up/sign-in-and-sign-up.component.jsx
+++ src/pages/sign-in-and-sign-up/sign-in-and-sign-up.component.jsx
@@ -1,12 +1,11 @@
-import React from 'react'
-import './sign-in-and-sign-up.styles.scss';
+import React from "react";
+import "./sign-in-and-sign-up.styles.scss";
+import SignIn from "../../components/sign-in/sign-in.component";
 
-// const SignInAndSignUpPage () => (
-//     return(
-//         <div className="sign-in-and-sign-up">
-//             SIGN IN
-//         </div>
-//     )
-// )
+const SignInAndSignUpPage = () => (
+  <div className="sign-in-and-sign-up">
+      <SignIn/>
+  </div>
+);
 
 export default SignInAndSignUpPage;
