963dc35ba4939dd142847edfad6a3002aa4dcd02
= <=>
2020-01-15 22:49:54 +0700

    Custom button component

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import "./sign-in.styles.scss";
 import FormInput from "../../pages/form-input/form-input.component";
+import CustomButton from "../custom-button/custom-button.component";
 
 class SignIn extends Component {
   constructor(props) {
@@ -48,7 +49,7 @@ class SignIn extends Component {
             required
           />
 
-          <input type="submit" value="Submit Form" />
+          <CustomButton type='submit'>Sign In</CustomButton>
         </form>
       </div>
     );
