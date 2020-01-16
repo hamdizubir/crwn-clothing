d9e42c9dd29e61cdf64aeeb1b6b1accb7ae57a98
Hamdi Zubir Koto <hamdizubir_k@Kotos-Mac-mini.local>
2020-01-16 16:19:29 +0700

    google sign in contd

--- src/App.js
+++ src/App.js
@@ -1,30 +1,50 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
 import {Route, Switch, Link} from 'react-router-dom';
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
 import Header from './components/header/header.component';
 import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
+import { auth } from './firebase/firebase.utils';
 
-const HatsPage = () =>(
-  <div>
-    <h1>HATS PAGE</h1>
-    <Link to="/">Homepage</Link>
-  </div>
-)
 
-function App() {
-  return (
-    <div className="App">
-      <Header/>
-      <Switch>
-        <Route exact path="/" component={HomePage}/>
-        <Route  path="/shop" component={ShopPage}/>
-        <Route  path="/signin" component={SignInAndSignUpPage}/>
-      </Switch>
+class App extends Component{
 
-    </div>
-  );
+  constructor(){
+    super();
+
+    this.state = {
+      currentUser: null,
+    }
+  }
+
+  unsubscribeFromAuth = null;
+
+  componentDidMount(){
+    this.unsubscribeFromAuth = auth.onAuthStateChanged(user => {
+      this.setState({currentUser: user});
+
+      console.log(user);
+    })
+  }
+
+  componentWillUnmount(){
+    this.unsubscribeFromAuth();
+  }
+
+  render(){
+    return (
+      <div className="App">
+        <Header/>
+        <Switch>
+          <Route exact path="/" component={HomePage}/>
+          <Route  path="/shop" component={ShopPage}/>
+          <Route  path="/signin" component={SignInAndSignUpPage}/>
+        </Switch>
+  
+      </div>
+    );
+  }
 }
 
 export default App;
