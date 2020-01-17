929df4a4471cf693fd5682a1fcbc2f405f7dc299
Hamdi Zubir Koto <hamdizubir_k@Kotos-Mac-mini.local>
2020-01-17 11:24:53 +0700

    Store data to firebase

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -11,6 +11,35 @@ const config = {
     messagingSenderId: "511784218159",
     appId: "1:511784218159:web:dedb54bcec2fd53240a0da",
     measurementId: "G-SNT07NNQ4C"
+};
+
+export const createUserProfileDocument  = async (userAuth, additionalData) => {
+    if(!userAuth){
+        return;
+    }
+
+    const userRef = firestore.doc(`users/${userAuth.uid}`);
+    const snapShot = await userRef.get();
+
+    if(!snapShot.exists){
+        const {displayName, email} = userAuth;
+        const createdAt = new Date();
+
+        try {
+            await userRef.set({
+                displayName,
+                email,
+                createdAt,
+                ...additionalData
+            })
+        } catch (error) {
+            console.log('error creating user', error.message);
+        }
+    }
+
+    return userRef;
+
+
 }
 
 firebase.initializeApp(config);
