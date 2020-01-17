ea3f0dc55476dfd6f7911a3e15dd0cd501972ed9
Hamdi Zubir Koto <hamdizubir_k@Kotos-Mac-mini.local>
2020-01-17 10:30:33 +0700

    Sign in page completed

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -50,11 +50,14 @@ class SignIn extends Component {
             //required
           />
 
-          <CustomButton type='submit'>Sign In</CustomButton>
-          <CustomButton onClick={SignInWithGoogle}>
-            {' '}
-            Sign In With Google{' '}
-          </CustomButton>
+          <div className='buttons'>
+            <CustomButton type='submit'>Sign In</CustomButton>
+            <CustomButton onClick={SignInWithGoogle} isGoogleSignIn>
+              {' '}
+              Sign In With Google{' '}
+            </CustomButton>
+          </div>
+
         </form>
       </div>
     );
